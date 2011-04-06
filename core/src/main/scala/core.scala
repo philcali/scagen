21b2db2cb25b77b4d585c140b327264bc88034bb
Philip Cali <philip.cali@gmail.com>
2011-04-05 21:42:05 -0500

    Library ready for usage.

--- core/src/main/scala/core.scala
+++ core/src/main/scala/core.scala
@@ -1,8 +1,31 @@
-package com.github.philcali
+package com.github.philcali.scagen
 
 import java.io.File
-import org.clapper.markwrap._ 
 import scala.io.Source.{fromFile => open}
+
+object Helpers {
+ def copy(oldFile: File, newFile: File) {
+    val reader = new java.io.FileInputStream(oldFile)
+    val writer = new java.io.FileOutputStream(newFile)
+  
+    copyStream(reader, writer)
+  }
+
+
+  def write(file: File, contents: String) {
+    val writer = new java.io.FileWriter(file)
+    writer.write(contents)
+    writer.close
+  }
+
+  def copyStream(in: java.io.InputStream, out: java.io.OutputStream) { 
+    val bytes = new Array[Byte](1024)
+    in read(bytes) match {
+      case n if(n > -1) => out.write(bytes, 0, n); copyStream(in, out)
+      case _ => in.close; out.close
+    }
+  }
+}
 
 trait Crawling[A] {
   val crawler: Crawler[A]
@@ -19,7 +42,6 @@ trait CrawlerConfiguration {
 
   def findFirst(what: String => Boolean) = 
     inputDir.listFiles.find(f => what(f.getName))
-  
 }
 
 trait StaticSiteConfiguration extends CrawlerConfiguration {
@@ -44,110 +66,8 @@ trait ConfiguredCrawler extends Crawling[File] {
       if(recursive)
         dirs foreach crawl
     }
-    def handler(file: File)
-  }
-}
-
-trait ParserCrawlerImpl extends ConfiguredCrawler {
-  this: StaticSiteConfiguration =>
-  
-  class ParserCrawler extends FileCrawler {
-    val styles = open(stylesheet).getLines.mkString
-    val template = open(baseTemplate).getLines.mkString("\n")
-    // My template replacer
-    val reg = """\#\{\s?(\w+)\s?\}\#""".r
-  
-    override def excluded = baseTemplate :: super.excluded
-
-    def handler(file: File) {
-      val name = file.getName
-      // Create this directory structure in output
-      val newPath = file.getCanonicalPath.replace(inputDir.getCanonicalPath + "/", "")
-      val newFolders = new File(outputDir, newPath.replace(name, ""))
-      if (!newFolders.exists) newFolders.mkdirs
-      
-      // Check file for conversion, copy all other contents over
-      try {
-        val parser = MarkWrap.parserFor(file)
-        val html = parser.parseToHTML(open(file))
-        // Prepare template.
-        val tmpData = Map("contents" -> html, "styles" -> styles)
-        val contents = reg.findAllIn(template).foldLeft(template) { (temp, matched) =>
-          val reg(key) = matched
-          reg.replaceFirstIn(temp, tmpData.getOrElse(key, ""))
-        }
-        val switched = name.split("\\.")(0) + ".html"
-        write(new File(newFolders, switched), contents)
-      } catch {
-        case e: IllegalArgumentException =>
-          copy(file, new File(newFolders, name)) 
-      }
-    }
-  
-    // Copy binaries, or other things 
-    def copy(oldFile: File, newFile: File) {
-      val reader = new java.io.FileInputStream(oldFile)
-      val writer = new java.io.FileOutputStream(newFile)
-    
-      copyStream(reader, writer)
-    }
-
-
-    def write(file: File, contents: String) {
-      val writer = new java.io.FileWriter(file)
-      writer.write(contents)
-      writer.close
-    }
 
     def start = crawl(inputDir)
-  }
-
-  def copyStream(in: java.io.InputStream, out: java.io.OutputStream) { 
-    val bytes = new Array[Byte](1024)
-    in read(bytes) match {
-      case n if(n > -1) => out.write(bytes, 0, n); copyStream(in, out)
-      case _ => in.close; out.close
-    }
-  }
-}
-
-class Sitegen(input: String = ".", output: String = "converted", recursively: Boolean = false,
-              base: Option[String] = None, css: Option[String] = None) 
-              extends ParserCrawlerImpl 
-              with StaticSiteConfiguration {
-
-  // Default values
-  val defaultTemp = "base.tpl"
-  val defaultCss = "main.css"
-
-  // From configuration
-  val recursive = recursively
-  val inputDir = new File(input)
-  val outputDir = new File(output)
-  val baseTemplate = base.map (new File(_: String))
-                         .getOrElse (findFirst(_ == defaultTemp)
-                         .getOrElse (resource(defaultTemp))) 
-  val stylesheet = css.map (new File(_: String))
-                      .getOrElse (findFirst(_ == defaultCss)
-                      .getOrElse (resource(defaultCss)))
-
-  // Our Site gen requirements
-  require(inputDir.exists && inputDir.isDirectory)
-
-  if (outputDir.exists && outputDir.isFile) {
-    throw new IllegalArgumentException("%s must be a directory" format(outputDir))
-  }
-
-  // From Parser
-  val crawler = new ParserCrawler
-
-  // Pull defaults from classpath
-  def resource(what: String) = {
-    val contents = getClass.getClassLoader.getResourceAsStream(what)
-    val file = new File(inputDir, what)
-    val out = new java.io.FileOutputStream(file)
-
-    copyStream(contents, out)
-    file
+    def handler(file: File)
   }
 }
